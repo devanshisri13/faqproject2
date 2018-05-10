895369298243ea334efc8f27c86141b59e46ba7b
devanshisri <ds846@njit.edu>
2018-05-09 23:28:41 -0400

    Added unit test for current month Questions.

--- tests/Unit/QuestionTest.php
+++ tests/Unit/QuestionTest.php
@@ -5,7 +5,8 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
+use App\Question;
+use DB;
 class QuestionTest extends TestCase
 {
     /**
@@ -15,10 +16,21 @@ class QuestionTest extends TestCase
      */
     public function testSave()
     {
-        $user = $user = factory(\App\User::class)->make();
+        $user= factory(\App\User::class)->make();
         $user->save();
         $question = factory(\App\Question::class)->make();
         $question->user()->associate($user);
         $this->assertTrue($question->save());
+    }
+
+    public  function testMonthlyQuestions()
+    {
+
+        $questions = DB::table('questions')
+            ->whereMonth('created_at', '05')
+            ->orderBy('created_at', 'desc')
+            ->take(5)
+            ->get();
+        $this->assertTrue($questions->save());
     }
 }
